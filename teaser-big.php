f522a2d1f9c097e881463ad0a4f8a1348c816ba0
Kim Meyer <kim.meyer@palasthotel.de>
2016-04-28 14:10:14 +0200

    get same  image format for teaser big as in article

--- teaser-big.php
+++ teaser-big.php
@@ -6,6 +6,22 @@
 
 $teaser_classes = array( 'ph-teaser', 'ph-teaser-big' );
 
+$thumbnail_id = get_post_thumbnail_id();
+
+// Default thumbnail image size
+$image_format = 'teaser-big-desktop-2x';
+
+// If original image is smaller than default thumbnail image size,
+// get a smaller image size
+$thumbnail_image_src = wp_get_attachment_image_src( $thumbnail_id, $image_format );
+$min_width = digitalepracht_image_sizes()[ $image_format ][ 'width' ];
+$min_height = digitalepracht_image_sizes()[ $image_format ][ 'height' ];
+
+if ( $thumbnail_image_src[ 1 ] < $min_width ||
+     $thumbnail_image_src[ 2 ] < $min_height ) {
+	$image_format = 'teaser-illustrated-mobile-max-2x';
+}
+
 ?>
 <article id="<?php echo $post->ID; ?>" <?php post_class( $teaser_classes ); ?>>
 	<a href="<?php echo get_permalink( $post->ID ); ?>" class="ph-teaser-link ph-clearfix"
@@ -13,7 +29,7 @@ $teaser_classes = array( 'ph-teaser', 'ph-teaser-big' );
 		<h2 class="ph-teaser-title"><?php the_title(); ?></h2>
 		<?php if ( has_post_thumbnail() ) : ?>
 			<figure class="ph-teaser-image-wrapper">
-				<?php the_post_thumbnail( 'teaser-big-desktop-2x', array( 'class' => 'ph-teaser-image' ) ); ?>
+				<?php the_post_thumbnail( $image_format, array( 'class' => 'ph-teaser-image' ) ); ?>
 			</figure>
 		<?php endif; ?>
 		<div class="ph-teaser-text"><?php the_excerpt(); ?></div>
