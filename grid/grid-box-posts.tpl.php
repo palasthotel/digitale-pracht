2e13efb1e4a6602016ea0c958b238b33d09e0ca2
Kim Meyer <kim.meyer@palasthotel.de>
2016-09-26 14:37:24 +0200

    grid box posts template update

---
+++ grid/grid-box-posts.tpl.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @author Kim-Christian Meyer <kim.meyer@palasthotel.de>
+ *
+ * @package digitale-pracht
+ */
+
+$classes = $this->classes;
+if ( ! empty( $this->style ) ) {
+	array_push( $classes, $this->style );
+}
+if ( empty( $this->title ) ) {
+	array_push( $classes, 'has-no-title' );
+} else {
+	array_push( $classes, 'has-title' );
+}
+
+?>
+<div class="grid-box <?php echo esc_attr( implode( $classes, ' ' ) ); ?>">
+	<?php if ( ! empty( $this->title ) ) : ?>
+		<?php if ( ! empty( $this->titleurl ) ) : ?>
+			<h2 class="grid-box-title"><a class="grid-box-title-link"
+			                              href="<?php echo esc_attr( $this->titleurl ); ?>"><?php echo esc_html( $this->title ); ?></a>
+			</h2>
+		<?php else: ?>
+			<h2 class="grid-box-title"><?php echo esc_html( $this->title ); ?></h2>
+		<?php endif; ?>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $this->prolog ) ) : ?>
+		<div class="grid-box-prolog">
+			<?php echo $this->prolog; ?>
+		</div>
+	<?php endif; ?>
+
+	<?php
+	// START of WordPress Loop
+	$query = new WP_Query( $content );
+	while ( $query->have_posts() ) {
+		$query->the_post();
+
+		/**
+		 * if avoid doublets plugin is activated
+		 */
+		if ( function_exists( 'grid_avoid_doublets_add' ) ) {
+			grid_avoid_doublets_add( get_the_ID(), $this->grid->gridid );
+		}
+
+		$found = false;
+		// Checks if WordPress has a template for post content ...
+		if ( $this->storage->templatesPath !== null ) {
+			$template_path = trailingslashit( $this->storage->templatesPath );
+			if ( file_exists( $template_path . 'post_content.tpl.php' ) ) {
+				$found = true;
+				include $template_path . 'post_content.tpl.php';
+			}
+		}
+		// ... if not, uses Grid template for post content
+		if ( ! $found ) {
+			include dirname( __FILE__ ) . '/post_content.tpl.php';
+		}
+	}
+	wp_reset_postdata();
+	// END of WordPress Loop
+	?>
+
+	<?php if ( ! empty( $this->epilog ) ) : ?>
+		<div class="grid-box-epilog">
+			<?php echo $this->epilog ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $this->readmore ) ) : ?>
+		<a href="<?php echo esc_url( $this->readmoreurl ); ?>"
+		   class="grid-box-readmore-link"><?php echo esc_html( $this->readmore ); ?></a>
+	<?php endif; ?>
+
+</div>
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: Palasthotel <rezeption@palasthotel.de> (in person: Kim-Christian Meyer)
 Requires at least: WordPress 4.0
 Tested up to: WordPress 4.6
-Version: 1.9
+Version: 1.10
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: white, light, one-column, responsive-layout, custom-colors, featured-images, microformats, theme-options, threaded-comments, translation-ready
