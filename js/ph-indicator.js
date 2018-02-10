c22a8db05c10bc7a8b348cc65801488d167f7d54
Kim Meyer <kim.meyer@palasthotel.de>
2018-02-10 22:44:45 +0100

    performance improvement for reading indicator

--- js/ph-indicator.js
+++ js/ph-indicator.js
@@ -3,8 +3,6 @@
  * It handles scroll, resize and load event and sets the height of indicatorDomEl
  * accordingly to the height of contentDomEl.
  *
- * @requires ph-debounce.js
- *
  * @author Kim-Christian Meyer <kim.meyer@palasthotel.de>
  */
 
@@ -12,8 +10,6 @@
 
 	ph.Indicator = function (indicatorDomEl, contentDomEl, timeoutScroll, timeoutResize) {
 		var _this = this;
-		this.timeoutScroll = typeof timeoutScroll === 'undefined' ? 20 : timeoutScroll;
-		this.timeoutResize = typeof timeoutResize === 'undefined' ? 150 : timeoutResize;
 		this.indicatorDomEl = indicatorDomEl;
 		this.contentDomEl = contentDomEl;
 
@@ -26,30 +22,24 @@
 			}
 
 			_this.contentHeight = _this.contentDomEl.offsetHeight;
-			_this.setIndicatorHeight();
+			requestAnimationFrame(_this.setIndicatorHeight);
 		};
 
 		this.init = function () {
-			// For requestAnimationFrame handling
-			_this.latestKnownScrollY = 0;
-			_this.ticking = false;
-
 			_this.initDimensions();
-			window.addEventListener('scroll', _this.rafScroll);
-			window.addEventListener('resize', _this.debounceInitDimensions);
+			window.addEventListener('scroll', function() {
+				requestAnimationFrame(_this.setIndicatorHeight);
+			});
+			window.addEventListener('resize', _this.initDimensions);
 		};
 
 		this.setIndicatorHeight = function () {
-			// Reset the tick so we can
-			// Capture the next onScroll
-			_this.ticking = false;
-
-			_this.indicatorDomEl.style.height = _this.readingCompleted() * 100 + '%';
+			_this.indicatorDomEl.style.transform = `scaleY(${_this.readingCompleted()})`;
 		};
 
 		// Returns a ratio value between 0 and 1
 		this.readingCompleted = function () {
-			var viewPortScrollYBottom = _this.latestKnownScrollY + document.documentElement.clientHeight;
+			var viewPortScrollYBottom = window.scrollY + document.documentElement.clientHeight;
 			var viewPortScrollYBottomWithoutContentTop = viewPortScrollYBottom - _this.contentTop;
 			var ratio = viewPortScrollYBottomWithoutContentTop / _this.contentHeight;
 			if (ratio > 1) {
@@ -61,22 +51,6 @@
 			return ratio;
 		};
 
-		this.rafScroll = function () {
-			_this.latestKnownScrollY = window.scrollY;
-			_this.requestTick();
-		};
-
-		this.requestTick = function () {
-			if (!_this.ticking) {
-				requestAnimationFrame(_this.setIndicatorHeight);
-			}
-			_this.ticking = true;
-		};
-
-		this.debounceInitDimensions = ph.debounce(function (e) {
-			_this.initDimensions();
-		}, this.timeoutResize);
-
 		// Don’t do anything, when requestAnimationFrame is not supported.
 		if (typeof window.requestAnimationFrame === 'function') {
 			window.addEventListener('load', this.init);
@@ -86,17 +60,13 @@
 
 	// Init reading indicator
 	document.addEventListener('DOMContentLoaded', function(e) {
-		var indicator,
-			articleTextDom = document.querySelector('.ph-article-text');
-		if (articleTextDom) {
-			indicator = new ph.Indicator(
-				document.getElementById('indicator'),
-				articleTextDom, 0);
-		}
-		else {
-			indicator = new ph.Indicator(
-				document.getElementById('indicator'),
-				document.getElementById('content'), 0);
+		var indicatorEl, contentEl;
+
+		indicatorEl = document.getElementById('indicator');
+		contentEl = document.querySelector('.ph-article-text') !== null ?
+			document.querySelector('.ph-article-text') : document.getElementById('content');
+		if (indicatorEl !== null && contentEl !== null) {
+			indicator = new ph.Indicator(indicatorEl, contentEl);
 		}
 	});
 
